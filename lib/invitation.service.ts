214c0f40b899cae06ce69c05b67acbef16179795
AlexTraveylan <monsieurdemares@gmail.com>
2025-02-16 12:30:39 +0100

    Add invitation service with URL key generation function

---
+++ lib/invitation.service.ts
@@ -0,0 +1,11 @@
+import crypto from "crypto"
+
+export function createInvitationUrlKey(): string {
+  const randomBytes = crypto.randomBytes(16)
+
+  return randomBytes
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "")
+}

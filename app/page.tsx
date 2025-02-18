a1bb69a0fa528628199e4dcb1717ac4c6f9e5373
AlexTraveylan <monsieurdemares@gmail.com>
2025-02-18 23:51:23 +0100

    Add link to parent list in homepage; enhance layout with centered styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link"
 import NewsletterForm from "./(composants)/newsletter-form"
 
 export default function Home() {
-  return <NewsletterForm />
+  return (
+    <>
+      <NewsletterForm />
+      <div className="flex justify-center pt-8 text-muted-foreground hover:text-primary transition-colors">
+        <Link href="/contact">{"Voir la liste des parents Élus"}</Link>
+      </div>
+    </>
+  )
 }
